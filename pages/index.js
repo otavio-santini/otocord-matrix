c3672a86065fa0a6909139383e7650a4ed8e156a
otavio-santini <97420060+otavio-santini@users.noreply.github.com>
2022-01-25 23:48:00 -0300

    my first update

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ function Titulo(props){
 //   export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'peas';
+    const username = 'elonmuskceo';
   
     return (
       <>
@@ -71,8 +71,9 @@ export default function PaginaInicial() {
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
             backgroundColor: appConfig.theme.colors.primary[500],
-            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
-            backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+            // backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/07/earthrise.jpg)',
+            backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply', 
           }}
         >
           <Box
@@ -85,9 +86,9 @@ export default function PaginaInicial() {
                 sm: 'row',
               },
               width: '100%', maxWidth: '700px',
-              borderRadius: '5px', padding: '32px', margin: '16px',
+              borderRadius: '5px', padding: '32px', margin: '16px', border: 'solid', borderColor:appConfig.theme.colors.neutrals[700],
               boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-              backgroundColor: appConfig.theme.colors.neutrals[700],
+            //   backgroundColor: appConfig.theme.colors.neutrals[700],
             }}
           >
             {/* Formulário */}
@@ -98,7 +99,8 @@ export default function PaginaInicial() {
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
               }}
             >
-              <Titulo tag="h2">Boas vindas de volta!</Titulo>
+              {/* <Titulo tag="h2">Boas vindas de volta!</Titulo> */}
+              <Titulo tag="h2">{appConfig.otoLabels.otoWelcome}</Titulo>
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
@@ -116,7 +118,7 @@ export default function PaginaInicial() {
               />
               <Button
                 type='submit'
-                label='Entrar'
+                label={appConfig.otoLabels.otoButtonEnter}
                 fullWidth
                 buttonColors={{
                   contrastColor: appConfig.theme.colors.neutrals["000"],
